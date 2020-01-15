be0a63ec7e91bc9ea1ad605f3c6cc68270a5a99a
IG <ian.a.guitard@gmail.com>
2020-01-15 15:27:11 -0500

    Update json_appender
    -Changed .dump to .dumps due to issues with bottom of long files getting cut off

--- json_appender.py
+++ json_appender.py
@@ -1,23 +1,19 @@
-import json, pandas
+import json
 
 orig_json_path = open("PATH_TO_JSON.json", 'r')
-csv_path = open("PATH_TO_CSV.csv", 'r')
 new_file = open("new_data.json", "w")
+NEW_DATA = None
 
 my_json_data = json.load(orig_json_path)
-my_csv_data = pandas.read_csv(csv_path)
-
-counter = 0
 
 for x in my_data:
-    '''
-    This specific situation involved adding a new column of data
-    which was mistakenly omitted when the original json file was created.
-    As such, this loop assumes a 1:1 relationship between the lengths of each
-    file.
-    '''
-    my_json_data[x]['NEW_COLUMN'] = my_new_data.imageFileName[counter]
+    """
+    Update NEW_COLUMN and NEW_DATA accordingly
+    """
+    my_json_data[x]['NEW_COLUMN'] = NEW_DATA
     counter += 1
 
 #Create new json with proper formatting
-json.dump(my_data, new_file, indent = 4)
+    #Uses .dumps due to line cutoff issues when using .dump
+json_str = json.dumps(my_data, indent = 4)
+new_file.write(json_str)
